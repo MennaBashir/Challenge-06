1886e588bc63b9467b4944c8f61a0e9bc7a1f927
MennaBashir <150310423+MennaBashir@users.noreply.github.com>
2024-01-04 19:02:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-
+let T;
 let func = setInterval(function () {
     let t1 = new Date("Dec 18,2023 23:59:59");
     let t2 = new Date();
-    let T = t1 - t2;
+     T = t1 - t2;
     document.querySelector("#day").innerHTML = (`0${Math.floor(T / (1000 * 60 * 60 * 24))}`);
     let x = T % (1000 * 60 * 60 * 24);
     document.querySelector("#hour").innerHTML = (`${Math.floor(x / (1000 * 60 * 60))}`);
