0219ca830b990d265967943606d118c7328da730
MennaBashir <150310423+MennaBashir@users.noreply.github.com>
2024-01-04 19:57:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-let T;
-let func = setInterval(function () {
-    let t1 = new Date("Jan 4,2024 23:59:59");
+function func () {
+    let t1 = new Date("Jan 5,2024 23:59:59");
     let t2 = new Date();
-     T = t2 - t1;
+     let T = t1 - t2;
     document.querySelector("#day").innerHTML = (`0${Math.floor(T / (1000 * 60 * 60 * 24))}`);
     let x = T % (1000 * 60 * 60 * 24);
     document.querySelector("#hour").innerHTML = (`${Math.floor(x / (1000 * 60 * 60))}`);
@@ -10,10 +9,10 @@ let func = setInterval(function () {
     document.querySelector("#minuit").innerHTML = (`${Math.floor(y / (1000 * 60))}`);
     let z = y % (1000 * 60);
     document.querySelector("#second").innerHTML = (`${Math.floor(z / 1000)}`);
-    if(T<0)
-{
-    clearInterval(func);
-}
-}, 1000);
+    if(T < 0)
+  { 
+     clearInterval(St);
+  }
+};
 
-
+let St=setInterval(func,1000);
